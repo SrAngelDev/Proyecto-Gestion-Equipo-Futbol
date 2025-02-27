b7b4bb9e9c07e4610d8926352904caacb263637e
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2025-02-27 16:49:14 +0100

    Comienzo de test de herencia

---
+++ src/test/kotlin/models/JugadoresTest.kt
@@ -0,0 +1,31 @@
+package models
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class JugadoresTest {
+    @Test
+    fun getPosicion() {
+    }
+
+    @Test
+    fun getDorsal() {
+    }
+
+    @Test
+    fun getAltura() {
+    }
+
+    @Test
+    fun getPeso() {
+    }
+
+    @Test
+    fun getGoles() {
+    }
+
+    @Test
+    fun getPartidosJugados() {
+    }
+
+}
