ea8129161078adda5164b855fbd7d93b0b9321b5
JorgeMrj <roadtoello2@gmail.com>
2025-02-27 16:22:19 +0100

    cache terminada falta test

--- src/main/kotlin/Cache/CacheImpl.kt
+++ src/main/kotlin/Cache/CacheImpl.kt
@@ -3,35 +3,43 @@ package srangeldev.Cache
 class CacheImpl<K,V>(
     private val capacidad: Int
 ): Cache<K, V> {
-    override fun get(key: K): V? {
-        TODO("Not yet implemented")
+    private val cache = object : LinkedHashMap<K, V>(
+        capacidad, 0.75f,
+    ) {
+        override fun removeEldestEntry(antiguo: MutableMap.MutableEntry<K, V>?): Boolean {
+            return size > capacidad
+        }
     }
+        override fun get(key: K): V? {
+            return cache[key]
+        }
+        override fun remove(key: K): V? {
+            return cache.remove(key)
+        }
 
-    override fun put(key: K, value: V): V? {
-        TODO("Not yet implemented")
-    }
+        override fun put(key: K, value: V): V? {
+            return cache.put(key, value)
+        }
 
-    override fun remove(key: K): V? {
-        TODO("Not yet implemented")
-    }
+        override fun keys(): Set<K> {
+            return cache.keys
+        }
 
-    override fun clear() {
-        TODO("Not yet implemented")
-    }
+        override fun values(): Collection<V> {
+            return cache.values
+        }
 
-    override fun size(): Int {
-        TODO("Not yet implemented")
-    }
+        override fun clear() {
+            return cache.clear()
+        }
 
-    override fun keys(): Set<K> {
-        TODO("Not yet implemented")
-    }
+        override fun size(): Int {
+           return cache.size
+        }
 
-    override fun values(): Collection<V> {
-        TODO("Not yet implemented")
-    }
 
-    override fun entries(): Set<Map.Entry<K, V>> {
-        TODO("Not yet implemented")
+        override fun entries(): Set<Map.Entry<K, V>> {
+            return cache.entries
+        }
+
     }
-}
