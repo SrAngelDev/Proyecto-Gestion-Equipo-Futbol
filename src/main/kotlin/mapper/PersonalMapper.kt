07d1e16403e833d9488eff9b8a08e0f94940c149
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2025-03-01 15:43:22 +0100

    Dto y mapper finalizados con casi el 100% de los test

--- src/main/kotlin/mapper/PersonalMapper.kt
+++ src/main/kotlin/mapper/PersonalMapper.kt
@@ -1,20 +1,37 @@
 package srangeldev.mapper
 
 import srangeldev.dto.PersonalDto
+import srangeldev.models.Entrenador
 import srangeldev.models.Jugador
-import srangeldev.models.Personal
+import java.time.LocalDate
 
-fun Personal.toDto(): PersonalDto{
-    return PersonalDto(
+fun PersonalDto.toEntrenador(): Entrenador {
+    return Entrenador(
         id = this.id,
         nombre = this.nombre,
         apellidos = this.apellidos,
-        fechaNacimiento = this.fechaNacimiento.toString(),
-        fechaIncorporacion = this.fechaIncorporacion.toString(),
+        fechaNacimiento = LocalDate.parse(this.fechaNacimiento),
+        fechaIncorporacion = LocalDate.parse(this.fechaIncorporacion),
         salario = this.salario,
         paisOrigen = this.paisOrigen,
-        rol = if (PersonalDto is Jugador) "Jugador" else "Entrenador",
-        especializacion =  this.especializacion,
-        dorsal = this.dorsal,
+        especializacion = Entrenador.Especializacion.valueOf(this.especializacion.toString())
+    )
+}
+
+fun PersonalDto.toJugador(): Jugador {
+    return Jugador(
+        id = this.id,
+        nombre = this.nombre,
+        apellidos = this.apellidos,
+        fechaNacimiento = LocalDate.parse(this.fechaNacimiento),
+        fechaIncorporacion = LocalDate.parse(this.fechaIncorporacion),
+        salario = this.salario,
+        paisOrigen = this.paisOrigen,
+        posicion = Jugador.Posicion.valueOf(this.posicion.toString()),
+        dorsal = this.dorsal ?: 0,
+        altura = this.altura ?: 0.0,
+        peso = this.peso ?: 0.0,
+        goles = this.goles ?: 0,
+        partidosJugados = this.partidosJugados ?: 0
     )
 }
