d4b48784f5c93e0c7cccd3bb62d673b249714593
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2025-03-08 21:02:34 +0100

    Trabajando en el menu

--- src/main/kotlin/view/View.kt
+++ src/main/kotlin/view/View.kt
@@ -1,6 +1,10 @@
 package srangeldev.view
 
+import org.lighthousegames.logging.logging
+
 class View {
+    private val logger = logging()
+
     fun showMenu() {
         println("Bienvenido a la aplicación de Entrenadores")
         println("1. Cargar datos desde fichero según la especificación indicada.")
@@ -12,7 +16,7 @@ class View {
         println("7. Salir.")
 
         print("Seleccione una opción: ")
-        val opcion = readln().toInt(
+        val opcion = readln().toInt()
 
         when(opcion) {
             1 -> cargarDatos()
@@ -20,5 +24,38 @@ class View {
             3 -> actualizarMiembro()
             4 -> eliminarMiembro()
             5 -> copiarDatos()
+            6 -> realizarConsultas()
+            7 -> salir()
+            else -> println("Opción no válida.")
+        }
+    }
+
+    fun cargarDatos() {
+        logger.debug { "Cargando datos" }
+    }
+
+    fun crearMiembro() {
+        logger.debug { "Creando miembro" }
+    }
+
+    fun actualizarMiembro() {
+        logger.debug { "Actualizando miembro" }
+    }
+
+    fun eliminarMiembro() {
+        logger.debug { "Eliminando miembro" }
+    }
+
+    fun copiarDatos() {
+        logger.debug { "Copiando datos" }
+    }
+
+    fun realizarConsultas() {
+        logger.debug { "Realizando consultas" }
+    }
+
+    fun salir() {
+        logger.debug { "Saliendo de la aplicación" }
+        println("Saliendo de la aplicación")
     }
 }
